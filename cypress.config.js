7da317aa9b9750837ada84497eb6b0855e070b31
pzwierzchowski-realport <pawel@realport.co>
2023-01-21 18:14:33 +0100

    fixes

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'hrekfd',
   
   viewportHeight: 800,
   viewportWidth: 1200,
---
+++ cypress/e2e/TS2.cy.js
@@ -0,0 +1,22 @@
+import HomePage from '../page-objects/homePage'
+import CartPage from '../page-objects/cartPage';
+import OrderPage from '../page-objects/orderPage'
+
+
+describe('my first scenario', () => {
+  const homePage = new HomePage();
+  const cartPage = new CartPage();
+  const orderPage = new OrderPage();
+
+  it('should add product to cart and delete it from there', function(){
+    homePage.visitPage()
+    homePage.addProductToCart()
+    homePage.clickGoToCartFromProductButton()
+    cartPage.checkThatAddedProductIsInCart()
+    cartPage.clickGoToPaymentsButton()
+    orderPage.fillAllRequiredFields()
+    orderPage.clickOrderFinishButton()
+    orderPage.checkOrderFinished()
+
+  })
+})
